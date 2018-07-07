ab0ecd98948d716cb53b0b8591b642c86ce783f3
HP <HP@LAPTOP-4O518B8V>
2018-07-08 02:01:28 +0300

    المتغير

---
+++ Lesson 0002 var/Lesson 0002 var/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_0002_var
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int g = 9;
+            Console.WriteLine(g);
+            Console.ReadKey();
+        }
+    }
+}
