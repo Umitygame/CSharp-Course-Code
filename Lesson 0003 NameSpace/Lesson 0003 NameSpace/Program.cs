d4e1a9603406d5e49131819cbb9de9d3788980bd
HP <HP@LAPTOP-4O518B8V>
2018-07-08 02:05:41 +0300

    NameSpace CSharp

---
+++ Lesson 0003 NameSpace/Lesson 0003 NameSpace/Program.cs
@@ -0,0 +1,17 @@
+﻿//هذه مكتبات وكلها في نيم سبيس واحد ولو تلاحظ ان السيستم منور لن استدعيه من كنسول رايات
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_3_NameSpace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(3333);//systemعندما تكتب هذا الكود تنور وتستدعا المكتبه 
+            Console.ReadKey();//حتى تبقى النافذه امام المستخدم
+        }
+    }
