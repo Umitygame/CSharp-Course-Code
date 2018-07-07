fc70c719eef88f9074ab1b3b2c58756306108d52
HP <HP@LAPTOP-4O518B8V>
2018-07-08 02:47:51 +0300

    الرياضيات

---
+++ Lesson 0008 mathematics var/Lesson 0008 mathematics var/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_0008_mathematics_var
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = 19.3;
+            double num2 = 17.4;
+            Console.WriteLine(num1 + num2);
+            Console.WriteLine(num1 - num2);
+            Console.WriteLine(num1 * num2);
+            Console.WriteLine(num1 / num2);
+            Console.WriteLine(num1 % num2);
+            int num3 = 9, num4 = 98, r;
+            r = num3 / num4;
+            Console.WriteLine(r);
+
+            double num5 = 5;
+            num5 += 5;
+            num5 *= 5;
+            num5 -= 5;
+            num5 /= 5;
+            num5 %= 5;
+            Console.WriteLine(num5);
+            string type = "ali";
+            type += " ";
+            type += "mohamed" + " " + "omr";
+            Console.WriteLine(type);
+            Console.ReadKey();
+            // type -= " ";خطا لا يجوز فقط الجمع يرهم مع النصوص
+
+        }
+    }
+}
