87b5d7e411d0660f646a647b371b8d4f94f8d068
HP <HP@LAPTOP-4O518B8V>
2018-07-09 13:26:12 +0300

    Logic

---
+++ Lesson 0012 logic/Lesson 0012 logic/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_0012_logic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool and1 = true && true;
+            bool and2 = true && false;
+            Console.WriteLine(and1+" "+and2+"\n");
+            // Console.WriteLine(and1 + and2);خطا
+            //لا يجوز وضع المتغيرن من دون فراغ لانه لن يقبل 
+
+            bool or1=true||false;
+            Console.WriteLine(or1);
+
+            bool not = !true;// ! علامة النفي هي  
+            Console.WriteLine(not);
+
+
+        }
+    }
+}
