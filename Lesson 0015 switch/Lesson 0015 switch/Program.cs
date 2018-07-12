bb4fbf3452722c6bab829d249a334c8dfa4ea7e9
HP <HP@LAPTOP-4O518B8V>
2018-07-12 16:40:35 +0300

    الادخال والاخراج

---
+++ Lesson 0015 switch/Lesson 0015 switch/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_0015_switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter number 1-3");
+            string num=Console.ReadLine();
+
+        }
+    }
+}
