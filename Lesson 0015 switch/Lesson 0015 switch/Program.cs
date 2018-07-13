7d841bef6d4bf457c430b44b0307d04501c17555
HP <HP@LAPTOP-4O518B8V>
2018-07-13 15:31:48 +0300

    Switch العبارات الشرطيه

--- Lesson 0015 switch/Lesson 0015 switch/Program.cs
+++ Lesson 0015 switch/Lesson 0015 switch/Program.cs
@@ -10,9 +10,63 @@ namespace Lesson_0015_switch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("enter number 1-3");
-            string num=Console.ReadLine();
+          /*  Console.WriteLine("enter number 1-3");
+            string str=Console.ReadLine();
+            int num = int.Parse(str);
+            switch (num)
+            {
+                case 1:
+                    Console.WriteLine("one");
+                    break;
+                case 2:
+                    Console.WriteLine("two");
+                    break;
+                case 3:
+                    Console.WriteLine("three");
+                    break;
+                default:
+                    Console.WriteLine("error");
+                    break;*/
 
+
+
+
+
+                    Console.Write("enter number 1-4 : ");
+                    string str = Console.ReadLine();
+                    int num = int.Parse(str);
+            Console.Write("enter passward : ");
+            string str1 = Console.ReadLine();
+                    int passward = int.Parse(str1);
+            if (passward == 12344321)
+            {
+                switch (num * 2)
+                {
+                    case 2:
+                    case 8:
+                        if (num == 1)
+                            Console.WriteLine("one");
+                        else
+                            Console.WriteLine("four");
+
+                        break;
+                    case 4:
+                        Console.WriteLine("two");
+                        break;
+                    case 6:
+                        Console.WriteLine("three");
+                        break;
+                    default:
+                        Console.WriteLine("error");
+                        break;
+
+
+
+                }
+
+            }
+            else
+                Console.WriteLine("error");
         }
     }
 }
